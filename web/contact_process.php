4168fa845418531892c0b6cbe060513f09cea66f
root <root@localhost.localdomain>
2019-02-24 14:11:48 -0500

    Update

--- web/contact_process.php
+++ web/contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "ashish.dsouza@students.polytech.k12.de.us";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
